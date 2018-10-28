56bc3f14f0c9b409cead3788d92396071858c59a
chayon <chayon.ror@gmail.com>
2018-10-28 16:03:11 +0600

    access_controll association

--- app/models/approval/access_control.rb
+++ app/models/approval/access_control.rb
@@ -3,6 +3,7 @@ module Approval
     has_paper_trail
     self.table_name = :approval_access_controls
 
+    has_many :approval_access_control_roles, class_name: 'Approval::AccessControlRole', foreign_key: :approval_access_control_id
     has_many :approval_maker_access_control_roles, ->{where(access_type: Approval::AccessControlRole::access_types[:maker])}, class_name: 'Approval::AccessControlRole', foreign_key: :approval_access_control_id
     has_many :approval_checker_access_control_roles, ->{where(access_type: Approval::AccessControlRole::access_types[:checker])}, class_name: 'Approval::AccessControlRole', foreign_key: :approval_access_control_id
     has_many :maker_roles, class_name: 'Role', through: :approval_maker_access_control_roles
